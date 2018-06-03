682d09346c37b0412ed5ab893fe457fd3d339386
Chris O'Donnell <chris.bowdoin@gmail.com>
2018-06-02 22:00:53 -0400

    Add postgres component.

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,7 @@
 (ns user
   (:require [flowbot.config :as config]
-            [integrant.repl :as ig.repl]))
+            [integrant.repl :as ig.repl :refer [clear go halt prep init reset reset-all]]
+            ;; for integrant multimethods
+            flowbot.data.postgres))
 
 (ig.repl/set-prep! config/system)
---
+++ src/flowbot/data/postgres.clj
@@ -0,0 +1,60 @@
+(ns flowbot.data.postgres
+  (:require [integrant.core :as ig]
+            [cheshire.core :as json]
+            hugsql.adapter
+            hugsql.core
+            [camel-snake-kebab.core :as csk]
+            [camel-snake-kebab.extras :as csk-extras]
+            [clojure.java.jdbc :as jdbc])
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource
+           org.postgresql.util.PGobject))
+
+(defmethod ig/init-key :postgres/connection [_ {:keys [dbtype dbname host port user pass]}]
+  (let [url (str "jdbc:" dbtype "://" host ":" port "/" dbname)
+        pool {:datasource (doto (ComboPooledDataSource.)
+                            (.setDriverClass "org.postgresql.Driver")
+                            (.setJdbcUrl url)
+                            (.setUser user)
+                            (.setPassword pass))}]
+    pool))
+
+(defmethod ig/halt-key! :rainbot/db [_ pool]
+  (.close (:datasource pool)))
+
+(defn value-to-json-pgobject [value]
+  (doto (PGobject.)
+    (.setType "json")
+    (.setValue (json/encode value))))
+
+(extend-protocol jdbc/ISQLValue
+  clojure.lang.IPersistentMap
+  (sql-value [value] (value-to-json-pgobject value))
+
+  clojure.lang.IPersistentVector
+  (sql-value [value] (value-to-json-pgobject value)))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  PGobject
+  (result-set-read-column [pgobj metadata idx]
+    (let [type  (.getType pgobj)
+          value (.getValue pgobj)]
+      (case type
+        ("json" "jsonb") (json/decode value true)
+        :else value))))
+
+(def memoized-kebab-keyword-ulate (memoize csk/->kebab-case-keyword))
+
+(defn result-one-snake->kebab
+  [this result options]
+  (->> (hugsql.adapter/result-one this result options)
+       (csk-extras/transform-keys memoized-kebab-keyword-ulate)))
+
+(defn result-many-snake->kebab
+  [this result options]
+  (->> (hugsql.adapter/result-many this result options)
+       (mapv #(csk-extras/transform-keys memoized-kebab-keyword-ulate %))))
+
+(defmethod hugsql.core/hugsql-result-fn :1 [sym] 'rainbot.db/result-one-snake->kebab)
+(defmethod hugsql.core/hugsql-result-fn :one [sym] 'rainbot.db/result-one-snake->kebab)
+(defmethod hugsql.core/hugsql-result-fn :* [sym] 'rainbot.db/result-many-snake->kebab)
+(defmethod hugsql.core/hugsql-result-fn :many [sym] 'rainbot.db/result-many-snake->kebab)
