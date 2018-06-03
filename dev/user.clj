ef48adc02fa3240509f3af49252e02dd135e220d
Chris O'Donnell <chris.bowdoin@gmail.com>
2018-06-03 11:39:57 -0400

    Add nrepl deps to dev environment.

--- dev/user.clj
+++ dev/user.clj
@@ -2,10 +2,15 @@
   (:require [flowbot.config :as config]
             [integrant.repl :as ig.repl :refer [clear go halt prep init reset reset-all]]
             [integrant.repl.state :refer [system]]
+            [cider-nrepl.main]
             ;; for integrant multimethods
             flowbot.data.postgres
             flowbot.discord.bot
             flowbot.discord.message-bus
-            flowbot.interceptor.registrar))
+            flowbot.interceptor.registrar
+            flowbot.effect.registrar))
 
 (ig.repl/set-prep! config/system)
+
+(cider-nrepl.main/init ["cider.nrepl/cider-middleware"])
+
